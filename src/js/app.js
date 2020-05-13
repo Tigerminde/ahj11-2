9443e27160cddd8f5a2ee74d703ac498e404e930
AppVeyor <ci@appveyor.com>
2020-05-13 16:20:18 +0300

    first commit

---
+++ src/js/app.js
@@ -0,0 +1,37 @@
+import { ajax } from 'rxjs/ajax';
+import { pluck, catchError, switchMap } from 'rxjs/operators';
+import { Observable, combineLatest, of } from 'rxjs';
+import AddMsg from './AddMsg.js';
+
+const elMessages = document.querySelector('.incom-messages');
+const addMsg = new AddMsg(elMessages);
+const url = 'https://coursar-heroku.herokuapp.com/';
+// const url = 'http://localhost:7070/posts';
+
+function loadPostComments(postsData) {
+  return new Observable((observer) => {
+    for (const item of postsData) {
+      const Aj = ajax.getJSON(`${url}/${item.id}/comments/latest`);
+      // let tempComm = {};
+      Aj.subscribe((value) => {
+        item.comments = value.data;
+        // tempComm = item;
+        observer.next(item);
+      });
+    }
+  });
+}
+
+(() => {
+  ajax
+    .getJSON(`${url}/latest`)
+    .pipe(
+      pluck('data'),
+      catchError(() => of([])),
+      switchMap((posts) => combineLatest(loadPostComments(posts))),
+    )
+    .subscribe((result) => {
+      addMsg.addMessages(...result);
+      // console.log(...result);
+    });
+})();
